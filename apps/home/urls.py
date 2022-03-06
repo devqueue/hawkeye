e9a74fba13838d2de5034d0d2c41001263a009a4
devqueue <haziq.sayyed@gmail.com>
2022-03-06 16:47:26 +0530

    search and upload urls included in home urls

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,15 +3,17 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from apps.home import views
 
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-
+    # upload app
+    path('',  include('apps.upload.urls')),
+    # search app
+    path('',  include('apps.search.urls')),
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
-
-]
+] 
