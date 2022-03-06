e061935f7f0c42fb6e66cc2a565dc42c90aca4e5
devqueue <haziq.sayyed@gmail.com>
2022-03-06 13:23:52 +0530

    added primary key for csv model

--- apps/upload/models.py
+++ apps/upload/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Csv(models.Model):
+    id = models.AutoField(primary_key=True) 
     file_name = models.FileField(upload_to='upload')
     uploaded = models.DateTimeField(auto_now_add=True)
     activated = models.BooleanField(default=False)
