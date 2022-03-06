1e5c9849f2cafd9e31677878c0e92da7c4c1584a
devqueue <haziq.sayyed@gmail.com>
2022-03-06 16:43:13 +0530

    search url and view added

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib import messages
+
 
 # Create your views here.
+
+
+# @login_required(login_url='accounts/login_user')
+def search_gene(request):
+    context = {
+        'segment': 'search'
+    }
+    return render(request, 'home/search.html', context)
