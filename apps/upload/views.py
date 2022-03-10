a5b9d1f8d0174ced0b26f7c452475556bf5015e4
devqueue <haziq.sayyed@gmail.com>
2022-03-10 13:11:06 +0530

    upload app completed

--- apps/upload/views.py
+++ apps/upload/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import CsvModelForm
 from .models import Csv
 import pandas as pd
+from apps.utils import count_het_hom, required
 from apps.search.models import GeneStorage
 # Create your views here.
 
@@ -10,6 +11,8 @@ from apps.search.models import GeneStorage
 @login_required(login_url='/login')
 def upload_file(request):
     form = CsvModelForm(request.POST or None, request.FILES or None)
+    is_csv = False
+    is_excel = False
     context = {
         'segment': 'upload',
         'form': form
@@ -21,9 +24,32 @@ def upload_file(request):
         obj = Csv.objects.get(activated=False)
         obj = Csv.objects.get(activated=False)
 
-        # TODO: populate the database
+        if (obj.file_name.path).endswith('.csv'):
+            is_csv = True
+        if (obj.file_name.path).endswith('.xlsx') or (obj.file_name.path).endswith('.xls'):
+            is_excel = True
+        # populate the database
         try:
-            df = pd.read_excel(obj.file_name.path)
+            if is_csv:
+                df = pd.read_csv(obj.file_name.path, low_memory=False)
+            elif is_excel:
+                df = pd.read_excel(obj.file_name.path, low_memory=False)
+
+            name = obj.file_name.path.split('/')[-1]
+            df.dropna(how='all', axis=1, inplace=True)
+            df.drop(df.columns.difference(required), axis=1, inplace=True)
+            df['filename'] = [name] * len(df)
+            grouped = df.groupby(['chromosome', 'start pos', 'end pos',
+                                    'reference']).agg(lambda x: x.tolist())
+
+            df = count_het_hom(grouped)  # FIXME: slow and main bottleneck
+            all_columns = list(df)  # Creates list of all column headers
+            df[all_columns] = df[all_columns].astype(str)
+            dff = df[df.columns.difference(['count_hom', 'count_het'])].replace("'", "", regex=True).replace("\[", "", regex=True).replace("\]", "", regex=True)
+            dff["count_hom"] = df['count_hom']
+            dff["count_het"] = df['count_het']
+            
+            dff = dff.reset_index()
 
             rows = [GeneStorage(
                 # id=record[" "],
@@ -33,24 +59,25 @@ def upload_file(request):
                 reference=record["reference"],
                 observed=record["observed"],
                 zygosity=record["zygosity"],
-                refGene_function=record["refGene function"],
+                refGene_function=record.get("refGene function", None),
                 refGene_gene=record["refGene gene"],
-                refGene_exonic_function=record["refGene exonic function"],
-                AC=record["AC"],
-                AC_hom=record["AC_hom"],
-                aug_all=record["1000g2015aug_all"],
-                ExAC_ALL=record["ExAC_ALL"],
-                gnomAD_exome_AF=record["gnomAD_exome_AF"],
-                Kaviar_AF=record["Kaviar_AF"],
-                SIFT_pred_41a=record["SIFT_pred_41a"],
-                SIFT4G_pred_41a=record["SIFT4G_pred_41a"],
-                Polyphen2_HDIV_pred_41a=record["Polyphen2_HDIV_pred_41a"],
-                Polyphen2_HVAR_pred_41a=record["Polyphen2_HVAR_pred_41a"],
-                CADD_phred_41a=record["CADD_phred_41a"],
-                CLNSIG=record["CLNSIG"],
-                count_hom=record["count hom"],
-                count_het=record["count het"]
-            )for record in df.to_dict('records')]
+                refGene_exonic_function=record.get("refGene exonic function", None),
+                AC=record.get("AC", None),
+                AC_hom=record.get("AC_hom", None),
+                aug_all=record.get("1000g2015aug_all", None),
+                ExAC_ALL=record.get("ExAC_ALL", None),
+                gnomAD_exome_AF=record.get("gnomAD_exome_AF", None),
+                Kaviar_AF=record.get("Kaviar_AF", None),
+                SIFT_pred_41a=record.get("SIFT_pred_41a", None),
+                SIFT4G_pred_41a=record.get("SIFT4G_pred_41a", None),
+                Polyphen2_HDIV_pred_41a=record.get("Polyphen2_HDIV_pred_41a", None),
+                Polyphen2_HVAR_pred_41a=record.get("Polyphen2_HVAR_pred_41a", None),
+                CADD_phred_41a=record.get("CADD_phred_41a", None),
+                CLNSIG=record.get("CLNSIG", None),
+                filename=record['filename'],
+                count_hom=record["count_hom"],
+                count_het=record["count_het"]
+            )for record in dff.to_dict('records')]
             try:
                 GeneStorage.objects.bulk_create(rows)
             except:
---
+++ apps/utils.py
@@ -0,0 +1,48 @@
+import imp
+
+
+import numpy as np
+
+
+def count_het_hom(grouped):
+    for index in grouped.index:
+        if len(grouped.loc[index]['zygosity']) == 1:
+            if grouped.loc[index]['zygosity'][0] == 'het':
+                grouped.at[index, 'count_het'] = 1
+            if grouped.loc[index]['zygosity'][0] == 'hom':
+                grouped.at[index, 'count_hom'] = 1
+        else:
+            length = len(grouped.loc[index]['zygosity'])
+            for i in range(length):
+                if grouped.loc[index]['zygosity'][i] == 'hom':
+                    grouped.at[index, 'count_hom'] = int(np.nan_to_num(
+                        grouped.at[index, 'count_hom']) + 1)
+                elif grouped.loc[index]['zygosity'][i] == 'het':
+                    grouped.at[index, 'count_het'] = int(np.nan_to_num(
+                        grouped.at[index, 'count_het']) + 1)
+    return grouped
+
+
+required = ["chromosome",
+            "start pos",
+            "end pos",
+            "reference",
+            "observed",
+            "zygosity",
+            "refGene function",
+            "refGene gene",
+            "refGene_exonic_function",
+            "AC",
+            "AC_hom",
+            "1000g2015aug_all",
+            "ExAC_ALL",
+            "gnomAD_exome_AF",
+            "Kaviar_AF",
+            "SIFT_pred_41a",
+            "SIFT4G_pred_41a",
+            "Polyphen2_HDIV_pred_41a",
+            "Polyphen2_HVAR_pred_41a",
+            "CADD_phred_41a",
+            "CLNSIG",
+            "count hom",
+            "count het"]
