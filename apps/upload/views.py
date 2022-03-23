1f1ecd28c01373483455421cd0bd9493c7bc316c
devqueue <haziq.sayyed@gmail.com>
2022-03-23 11:55:06 +0530

    completed compute and upload view

--- apps/upload/views.py
+++ apps/upload/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from core.settings import UPLOAD_PATH
 from .forms import CsvModelForm
 from .models import Csv
 import pandas as pd
-import numpy as np
-from apps.utils import count_het_hom, required, allowed_users
+from apps.utils import required, allowed_users
 from apps.search.models import GeneStorage
 from datetime import datetime
 import traceback
+import os
 # Create your views here.
 
 
@@ -21,7 +22,7 @@ def upload_file(request):
         'segment': 'upload',
         'form': form
     }
-    start = datetime.now()
+
     if form.is_valid():
         context['post'] = True
         form.save()
@@ -33,103 +34,31 @@ def upload_file(request):
         if (obj.file_name.path).endswith('.xlsx') or (obj.file_name.path).endswith('.xls'):
             is_excel = True
         
-        # populate the database
-        
         try:
             if is_csv:
-                df1 = pd.read_csv(obj.file_name.path, low_memory=False)
+                headers = pd.read_csv(obj.file_name.path, nrows=0).columns.tolist()
+                req = set(headers) & set(required)
+                df1 = pd.read_csv(obj.file_name.path, usecols=req, low_memory=False)
             elif is_excel:
-                df1 = pd.read_excel(obj.file_name.path, low_memory=False)
+                headers = pd.read_excel(obj.file_name.path,
+                                      nrows=0).columns.tolist()
+                req = set(headers) & set(required)               
+                df1 = pd.read_excel(obj.file_name.path, usecols=req, low_memory=False)
 
-            name = obj.file_name.path.split('/')[-1]
-            df1.dropna(how='all', axis=1, inplace=True)
-            df1.drop(df1.columns.difference(required), axis=1, inplace=True)
             df1.columns = df1.columns.str.replace(' ', '_')
-            df1['filename'] = [name] * len(df1)
+            test = df1[['chromosome', 'start_pos', 'end_pos', 'observed', 'refGene_gene', 'zygosity']]
 
-
-            # TODO: get all objects from the database
-            gene_already = GeneStorage.objects.all().values()
-            df_old = pd.DataFrame(gene_already)
-            print(df_old)
-            print(df1)
-            df = pd.concat([df_old, df1])
-
-            grouped = df.groupby(['chromosome', 'start_pos', 'end_pos',
-                                  'observed']).agg(lambda x: x.tolist())
-
-            df = count_het_hom(grouped)  # FIXME: slow and main bottleneck
-            all_columns = list(df)  # Creates list of all column headers
-
-            df[all_columns] = df[all_columns].astype(str)
-            dff = df.replace("'", "", regex=True).replace("\[", "", regex=True).replace("\]", "", regex=True).replace("\,", ";", regex=True)
-            print("After replace")
-            print(dff)
-            convert_dict = {'count_hom': float,
-                            'count_het': float,
-                            }
-            dff = dff.astype(convert_dict)
-
-            dff['count_het'].fillna(0, inplace=True)
-            dff['count_hom'].fillna(0, inplace=True)
-            dff['count_total'] = dff['count_hom'] + dff['count_het']
-
-
-            dff['files_uploaded'] = [len(dff['filename'].unique())] * len(dff)
-            dff['New_allele_frequency'] = (dff['count_het'] + 2*dff['count_hom']) / (dff['files_uploaded'] * 2)
-
-
-            dff = dff.reset_index()
-            print(dff)
-            print(dff.info())
-            # TODO: check if file name exists in database:
-
-            rows = [GeneStorage(
-                # id=record[" "],
-                chromosome=record["chromosome"],
-                start_pos=record["start_pos"],
-                end_pos=record["end_pos"],
-                reference=record["reference"],
-                observed=record["observed"],
-                zygosity=record["zygosity"],
-                refGene_function=record.get("refGene_function", None),
-                refGene_gene=record["refGene_gene"],
-                quality=record.get('quality', None),
-                refGene_exonic_function=record.get("refGene_exonic_function", None),
-                AC=record.get("AC", None),
-                AC_hom=record.get("AC_hom", None),
-                aug_all=record.get("1000g2015aug_all", None),
-                ExAC_ALL=record.get("ExAC_ALL", None),
-                gnomAD_exome_AF=record.get("gnomAD_exome_AF", None),
-                Kaviar_AF=record.get("Kaviar_AF", None),
-                SIFT_pred_41a=record.get("SIFT_pred_41a", None),
-                SIFT4G_pred_41a=record.get("SIFT4G_pred_41a", None),
-                Polyphen2_HDIV_pred_41a=record.get("Polyphen2_HDIV_pred_41a", None),
-                Polyphen2_HVAR_pred_41a=record.get("Polyphen2_HVAR_pred_41a", None),
-                CADD_phred_41a=record.get("CADD_phred_41a", None),
-                CLNSIG=record.get("CLNSIG", None),
-                filename=record['filename'],
-                count_hom=record["count_hom"],
-                count_het=record["count_het"],
-                count_total=record['count_total'],
-                files_uploaded=record['files_uploaded'],
-                New_allele_frequency=record['New_allele_frequency']
-            ) for record in dff.to_dict('records')]
-
-            GeneStorage.objects.all().delete()
-            GeneStorage.objects.bulk_create(rows)
-            
-            # After sucessfully populating 
+            # After sucessfully uploading 
             context['message'] = 'File was uploaded sucessfully.'
             context['color'] = 2
         except Exception:
             traceback.print_exc()
-            context['message'] = 'An Error occured while processing the file, please make sure the file format is correct and it contains all required columns.'
+            context['message'] = 'An Error occured while uploading the file, please make sure the file format is correct and it contains all required columns.'
             context['color'] = 4
         obj.activated = True
         obj.save()
-    end = datetime.now()
-    print(end - start)
+
+    
     return render(request, 'home/upload.html', context)
 
 
@@ -139,25 +68,108 @@ def compute(request):
     context = {
         'segment': 'compute',
     }
+    if request.method == 'POST':
+        start = datetime.now()
+        context['post'] = True
+        file_list = [os.path.join(UPLOAD_PATH, i)
+                    for i in os.listdir(UPLOAD_PATH) if i.endswith('.csv')]
+        df_list = []
+        try:
+            for file in file_list:
+                headers = pd.read_csv(file, nrows=0).columns.tolist()
+                req = set(headers) & set(required)
+                df = pd.read_csv(file, usecols=req, low_memory=False)
+                df['filename'] = [file.split('/')[-1]] * len(df)
+                df_list.append(df)
 
-    result = GeneStorage.objects.all().values()
-    df = pd.DataFrame(result)
-    try:
-        df = df[['chromosome', 'start_pos', 'end_pos', 'observed', 'refGene_gene', 'zygosity',
-                'filename', 'count_hom', 'count_het', 'files_uploaded', 'New_allele_frequency']]
+            frame = pd.concat(df_list, axis=0, ignore_index=True)
 
-        convert_dict = {'count_hom': int,
-                    'count_het': int,
-                    'files_uploaded': int,
-                    'New_allele_frequency': int,
-                    }
 
-        df = df.astype(convert_dict)
-        df = df.sample(8)
-        context['df_header'] = list(df.columns)
-        context['df'] = df.to_dict('records')
-    except Exception:
-        traceback.print_exc()
-    
+            # def processing(frame):
+            frame.columns = frame.columns.str.replace(' ', '_')
+            grouped = frame.groupby(['chromosome', 'start_pos', 'end_pos',
+                                    'observed']).agg(lambda x: x.tolist())
 
-    return render(request, 'home/compute.html', context)
+            grouped = grouped.astype(str).replace(
+                "'", "", regex=True).replace("\[", "", regex=True).replace("\]", "", regex=True).replace("\,", ";", regex=True)
+
+
+            grouped['count_het'] = grouped['zygosity'].apply(lambda x: x.count('hom'))
+            grouped['count_hom'] = grouped['zygosity'].apply(lambda x: x.count('het'))
+
+            grouped['count_total'] = grouped['count_het'] + grouped['count_hom']
+            grouped['files_uploaded'] = [len(df_list)] * len(grouped)
+            grouped['New_allele_frequency'] = (grouped['count_het'] + 2*grouped['count_hom']) / (grouped['files_uploaded'] * 2)
+
+            dtypes = {
+                'count_het': 'int16',
+                'count_hom': 'int16',
+                'count_total': 'int16',
+                'files_uploaded': 'int16',
+                'New_allele_frequency': 'float32',
+            }
+            grouped = grouped.astype(dtypes)
+            grouped = grouped.reset_index()
+
+            rows = [GeneStorage(
+                    # id=record[" "],
+                    chromosome=record["chromosome"],
+                    start_pos=record["start_pos"],
+                    end_pos=record["end_pos"],
+                    reference=record["reference"],
+                    observed=record["observed"],
+                    zygosity=record["zygosity"],
+                    refGene_function=record.get("refGene_function", None),
+                    refGene_gene=record["refGene_gene"],
+                    quality=record.get('quality', None),
+                    refGene_exonic_function=record.get(
+                        "refGene_exonic_function", None),
+                    AC=record.get("AC", None),
+                    AC_hom=record.get("AC_hom", None),
+                    aug_all=record.get("1000g2015aug_all", None),
+                    ExAC_ALL=record.get("ExAC_ALL", None),
+                    gnomAD_exome_AF=record.get("gnomAD_exome_AF", None),
+                    Kaviar_AF=record.get("Kaviar_AF", None),
+                    SIFT_pred_41a=record.get("SIFT_pred_41a", None),
+                    SIFT4G_pred_41a=record.get("SIFT4G_pred_41a", None),
+                    Polyphen2_HDIV_pred_41a=record.get(
+                        "Polyphen2_HDIV_pred_41a", None),
+                    Polyphen2_HVAR_pred_41a=record.get(
+                        "Polyphen2_HVAR_pred_41a", None),
+                    CADD_phred_41a=record.get("CADD_phred_41a", None),
+                    CLNSIG=record.get("CLNSIG", None),
+                    filename=record['filename'],
+                    count_hom=record["count_hom"],
+                    count_het=record["count_het"],
+                    count_total=record['count_total'],
+                    files_uploaded=record['files_uploaded'],
+                    New_allele_frequency=record['New_allele_frequency']
+                    ) for record in grouped.to_dict('records')]
+
+            GeneStorage.objects.all().delete()
+            GeneStorage.objects.bulk_create(rows)
+            context['message'] = 'All files were processed sucessfully.'
+            context['color'] = 2
+        except Exception:
+            traceback.print_exc()
+            context['message'] = 'An Error occured while processing the file, please check the logs'
+            context['color'] = 4
+        end = datetime.now()
+        print(end - start)
+        return render(request, 'home/compute.html', context)
+
+    else:  
+        result = GeneStorage.objects.all().values()
+        df = pd.DataFrame(result)
+        try:
+            df = df[['chromosome', 'start_pos', 'end_pos', 'observed', 'zygosity',
+                    'filename', 'count_hom', 'count_het', 'files_uploaded', 'New_allele_frequency']]
+
+            df = df.sample(5)
+            context['df_header'] = list(df.columns)
+            context['df'] = df.to_dict('records')
+        except Exception:
+            traceback.print_exc()
+        
+
+        return render(request, 'home/compute.html', context)
