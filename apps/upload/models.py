0265a6e1097baff5f7479578e06c0460a4decdfe
devqueue <haziq.sayyed@gmail.com>
2022-04-02 21:01:23 +0530

    upload path changed to data/web/upload

--- apps/upload/models.py
+++ apps/upload/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from core.settings import UPLOAD_PATH
 # Create your models here.
 
 class Csv(models.Model):
     id = models.AutoField(primary_key=True) 
-    file_name = models.FileField(upload_to='media/upload/')
+    file_name = models.FileField()
     uploaded = models.DateTimeField(auto_now_add=True)
     activated = models.BooleanField(default=False)
 
